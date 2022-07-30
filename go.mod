13c55e9bfd18d11dcb927f49f3c3349b5d19bc7f
Dan Prince <1266011+danprince@users.noreply.github.com>
2022-07-30 08:53:51 +0200

    adds a file watcher in serve mode

--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/alecthomas/chroma v0.10.0 // indirect
 	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/yuin/goldmark-highlighting v0.0.0-20220208100518-594be1970594 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- serve.go
+++ serve.go
@@ -5,42 +5,107 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
+	"path"
+	"time"
 
 	"github.com/danprince/sietch/internal/errors"
+	"github.com/fsnotify/fsnotify"
 )
 
-func Serve(b *builder) {
-	var err error
+var buildErr error
 
+func Serve(b *builder) {
 	server := http.FileServer(http.Dir(b.outDir))
 
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		rebuild := strings.HasSuffix(r.URL.Path, "/") || strings.HasSuffix(r.URL.Path, ".html")
-
-		// Rebuild the site whenever an HTML file is requested
-		if rebuild {
-			b.reset()
-			dt, err := b.build()
-
-			if err != nil {
-				w.Header().Add("Content-type", "text/html")
-				w.WriteHeader(500)
-				w.Write([]byte(errors.FmtErrorHtml(err)))
-				fmt.Fprintln(os.Stderr, errors.FmtError(err))
-				return
-			}
-
-			fmt.Printf("built %d pages in %s", len(b.pages), dt)
+		if buildErr != nil {
+			w.Header().Add("Content-type", "text/html")
+			w.WriteHeader(500)
+			w.Write([]byte(errors.FmtErrorHtml(buildErr)))
+		} else {
+			server.ServeHTTP(w, r)
 		}
-
-		server.ServeHTTP(w, r)
 	}))
 
+	go watch(func(events []fsnotify.Event) []string {
+		var dt time.Duration
+		dt, buildErr = b.build()
+		defer b.reset()
+
+		if buildErr != nil {
+			fmt.Fprintln(os.Stderr, errors.FmtError(buildErr))
+			return []string{}
+		}
+
+		fmt.Printf("built %d pages in %s\n", len(b.pages), dt)
+
+		dirs := b.dirs
+		for index, dir := range b.dirs {
+			dirs[index] = path.Join(b.pagesDir, dir)
+		}
+
+		return dirs
+	})
+
 	fmt.Println("serving site at http://localhost:8000...")
-	err = http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func watch(build func(events []fsnotify.Event) []string) {
+	watcher, err := fsnotify.NewWatcher()
+	ticker := time.Tick(time.Millisecond * 100)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	var events []fsnotify.Event
+
+	// Trigger an initial build with no changes
+	watchDirs := build(events)
+
+	for _, dir := range watchDirs {
+		watcher.Add(dir)
+	}
+
+	for {
+		select {
+		case <-done:
+			return
+
+		case event := <-watcher.Events:
+			if event.Op != fsnotify.Chmod {
+				events = append(events, event)
+			}
+
+		case err := <-watcher.Errors:
+			log.Println(errors.FmtError(err))
+
+		case <-ticker:
+			if len(events) > 0 {
+				dirs := build(events)
+				events = []fsnotify.Event{}
+
+				for _, dir := range watcher.WatchList() {
+					watcher.Remove(dir)
+				}
+
+				for _, dir := range dirs {
+					watcher.Add(dir)
+				}
+			}
+		}
+	}
+}
+
+func fmtError(err error) {
+	panic("unimplemented")
 }
