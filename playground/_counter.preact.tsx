1230cc5f71e261aeb426ba6c6e96d06c8bf28632
Dan Prince <1266011+danprince@users.noreply.github.com>
2022-07-31 09:40:43 +0200

    clean up unused stuff in example components

--- playground/_counter.preact.tsx
+++ playground/_counter.preact.tsx
@@ -1,18 +1,12 @@
 // @ts-nocheck
-/** @jsx h */
-/** @jsxFrag Fragment */
-
 import "./_counter.css";
-import { h } from "preact";
-import { useState, useEffect } from "preact/hooks"
+import { useState } from "preact/hooks"
 
 let Counter = ({ count: initialCount = 0 }) => {
   let [count, setCount] = useState(initialCount);
 
   return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-    </div>
+    <button onClick={() => setCount(count + 1)}>{count}</button>
   );
 }
 
--- playground/_counter.react.tsx
+++ playground/_counter.react.tsx
@@ -1,15 +1,11 @@
 // @ts-nocheck
-/** @jsx React.createElement */
-/** @jsxFrag React.Fragment */
 import "./_counter.css";
 import React, { useState } from "react";
 
 let Counter = ({ count: initialCount = 0 }) => {
-  let [count, setCount] = useState(initialCount + 5);
+  let [count, setCount] = useState(initialCount);
   return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-    </div>
+    <button onClick={() => setCount(count + 1)}>{count}</button>
   );
 }
 
