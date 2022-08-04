301fdb7b34a0b741d429c2c98b76dd2cda01511f
Dan Prince <1266011+danprince@users.noreply.github.com>
2022-08-04 23:24:12 +0200

    removes built files

--- docs/_site/_assets/bundle-3ba3.js
+++
@@ -1,9 +0,0 @@
-// snippets/counter.ts
-var hydrate = ({ count }, element) => {
-  let btn = element.querySelector("button");
-  btn.onclick = () => btn.textContent = String(++count);
-};
-
-// browser-pages:3ba3?browser
-hydrate({ "count": 0 }, document.getElementById("3ba3_1"));
-//# sourceMappingURL=bundle-3ba3.js.map
