0b560541cefe6bc20cd7da51e7254ad548d966cf
Dan Prince <1266011+danprince@users.noreply.github.com>
2022-08-07 17:40:42 +0200

    removes unused islands testdata

--- internal/islands/testdata/preact-static.js
+++
@@ -1,8 +0,0 @@
-import { h } from 'preact';
-import { renderToString as render } from 'preact-render-to-string';
-import $ca from './Counter.tsx';
-$elements['a'] = render(h($ca, {"count":1}));
-import $cb from './Counter.tsx';
-$elements['b'] = render(h($cb, {"count":3}));
-import $cc from '../Timer.tsx';
-$elements['c'] = render(h($cc, {}));
--- internal/islands/testdata/vanilla-static.js
+++
@@ -1,6 +0,0 @@
-import { render as $ra } from './Counter.tsx';
-$elements['a'] = $ra({"count":1});
-import { render as $rb } from './Counter.tsx';
-$elements['b'] = $rb({"count":3});
-import { render as $rc } from '../Timer.tsx';
-$elements['c'] = $rc({});
